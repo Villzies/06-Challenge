27c1cf4ba89f66d45acf311b100424b37182315a
Villzies <gemberling.stephan@att.net>
2023-11-19 14:55:48 -0600

    Partially fixed api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+const weatherBtn = document.getElementById("get-weather")
+const weatherCard = document.getElementById("weather-card")
+let cityName = document.getElementById("city-name")
 
+const getWeather = function getWeather(event) {
+  event.preventDefault();
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${"Austin"}}&limit=${"1"}&appid=${"bf982edcf16eb54619c1e757f8e942ba"}`,{
+  method: "GET",
+  contentType: "application/json"
+})
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data[0]);
+  let weatherinfo = data[0].name;
+  weatherCard.textContent = weatherinfo;
+})
+
+};
+
+weatherBtn.addEventListener("click", getWeather);
